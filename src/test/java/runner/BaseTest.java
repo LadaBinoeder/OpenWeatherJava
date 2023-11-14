656f47f8193863d1a30a34ed43a1f246fd9581d1
LadaBinoeder <lada.binoeder@web.de>
2023-11-14 19:05:09 +0100

    next try

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -14,7 +14,7 @@ public abstract class BaseTest {
         driver = BaseUtils.createDriver();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--no-sandbox", "--remote-allow-origins=*", "--headless", "--window-size=1920,1080",
-                "--disable-dev-shm-usage");
+                "--disable-dev-shm-usage", "--remote-debugging-port=9222");
 
     }
     @AfterMethod
