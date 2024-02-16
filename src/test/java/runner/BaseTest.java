be4c4f99e547aa8b9d746ec2da0530b1154f8a17
LadaBinoeder <126483875+LadaBinoeder@users.noreply.github.com>
2024-02-16 12:38:56 +0100

    ReportUtils and TestUtils added. Customized logs added (#49)

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -2,9 +2,14 @@ package runner;
 
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 public abstract class BaseTest {
@@ -12,13 +17,25 @@ public abstract class BaseTest {
     private ChromeDriver driver;
     private WebDriverWait webDriverWait;
 
+    @BeforeSuite
+    protected void beforeSuite(ITestContext context) {
+
+        Reporter.log(ReportUtils.getReportHeader(context), true);
+    }
+
     @BeforeMethod
-    protected void beforeMethod() {
+    protected void beforeMethod(ITestResult result) {
+
         driver = BaseUtils.createDriver();
+        Reporter.log(ReportUtils.END_LINE, true);
+        Reporter.log("TEST RUN", true);
+        Reporter.log(ReportUtils.getClassName(result), true);
 
     }
     @AfterMethod
-    protected void afterMethod() {
+    protected void afterMethod(Method method, ITestResult result) {
+        Reporter.log(ReportUtils.getTestStatistics(method, result), true);
+
         driver.quit();
         webDriverWait = null;
 
