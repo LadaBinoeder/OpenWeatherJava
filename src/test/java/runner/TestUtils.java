c636d6a252e932dab1d6b7741aa1a54b10dd072c
LadaBinoeder <lada.binoeder@web.de>
2024-02-16 11:55:23 +0100

    ReportUtils and TestUtils added. Customized logs added

---
+++ src/test/java/runner/TestUtils.java
@@ -0,0 +1,11 @@
+package runner;
+
+public class TestUtils {
+
+    private final static String BASE_URL = "https://openweathermap.org/";
+    public static String getBaseUrl() {
+
+        return BASE_URL;
+
+    }
+}
